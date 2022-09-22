5f2c593d80a2ea5d2e568e102d10e8292ce9dc0a
mcncm <33966432+mcncm@users.noreply.github.com>
2022-09-22 15:49:23 -0400

    Stubbed out a naive simulation backend

--- src/simulation.ml
+++ src/simulation.ml
@@ -1,5 +1,3 @@
-let simulate _ = Utils.todo ()
-
 module type BACKEND = sig
   type qstate
   type cstate
@@ -21,3 +19,31 @@ end
 qreg x[10] --> there exists 10 qubits called  x0...x9
 creg y[10] --> there exists 10 bits called    x0...x9
 *)
+
+(* A demo simulation backend to implement `simulate` *)
+(* Naive in two ways: it's a very poor implementation, and it's probably not nice OCaml. *)
+module Naive_simulation = struct
+  type quantum_state = Complex.t array
+
+  (* I guess this should be impure because `array` is *)
+  let apply_gate (_ : Circuit.gate) (_ : quantum_state) = Utils.todo ()
+
+  let init_state (circ : Circuit.circuit) : quantum_state =
+    (* state array should be 2^n entries long *)
+    let len = Int.shift_left 0 circ.qbits in
+    (* All-zero state *)
+    let state = Array.make len Complex.zero in
+    (* effectfully set the |00...0> entry to 1 *)
+    Array.set state 0 Complex.one;
+    state
+
+  let output_state (circ: Circuit.circuit) : quantum_state =
+    let state = init_state (circ) in
+    List.iter (fun g -> apply_gate g state) circ.gates;
+    state
+
+end
+
+(* TODO: some kind of top-level entry point for simulation. *)
+(* Probably has a more interesting type than `circuit -> state`. *)
+let simulate (circ : Circuit.circuit) = Naive_simulation.output_state circ
