4022748e14cd3ecff876a8f4201e7134584a7eea
Arthur Correnson <arthur.correnson@gmail.com>
2022-09-23 16:18:25 +0200

    move the naive simulator in a module

--- src/simulation.ml
+++ src/simulation.ml
@@ -18,21 +18,3 @@ end = struct
     state
 end
 
-module NaiveBackend : BACKEND = struct
-  type qstate = Complex.t array
-  
-  let init qbits =
-    let len = 1 lsl qbits in
-    (* All-zero state *)
-    let state = Array.make len Complex.zero in
-    (* effectfully set the |00...0> entry to 1 *)
-    Array.set state 0 Complex.one;
-    state
-
-  let apply_gate (_ : Circuit.gate) (_ : qstate) = Utils.todo ()
-
-  let repr x = x
-end
-
-module NaiveSimulator = Make(NaiveBackend)
-
