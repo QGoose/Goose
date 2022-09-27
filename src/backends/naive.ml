b2c12120c81d1a31a812e0b961e7a3865547e429
Arthur Correnson <arthur.correnson@gmail.com>
2022-09-27 19:59:11 +0200

    More idiomatic OCaml code in Naive simulator

--- src/backends/naive.ml
+++ src/backends/naive.ml
@@ -2,19 +2,17 @@
 
 open Simulation
 
-let complex_two = Complex.{re = 2.0; im = 0.0}
-let one_over_sqrt_2 = Complex.(div one (sqrt complex_two))
-let two_pi = Complex.{re = 2.0 *. Float.pi; im = 0.0}
 
-module NaiveBackend : BACKEND with type qstate = Complex.t array = struct
+
+module NaiveBackend : BACKEND = struct
   type qstate = Complex.t array
   
-  let iterationIndices : int -> int -> int * int = fun i t ->
+  let iteration_indices (i : int) (t : int) : int * int =
     let mask = (1 lsl t) - 1 in
     let notMask = lnot mask in
-    let firstIndex = (i land mask) lor ((i land notMask) lsl 1) in
-    let secondIndex = (firstIndex) lor (1 lsl t) in
-    (firstIndex, secondIndex) 
+    let i1 = (i land mask) lor ((i land notMask) lsl 1) in
+    let i2 = i1 lor (1 lsl t) in
+    (i1, i2)
 
   let init qbits =
     let len = 1 lsl qbits in
@@ -24,14 +22,30 @@ module NaiveBackend : BACKEND with type qstate = Complex.t array = struct
     Array.set state 0 Complex.one;
     state
 
-  let controls_check (state_index: int) (controls: Circuit.adr list): bool = 
-    List.fold_left (&&) true (List.map (fun (Circuit.A c) -> ((1 lsl c) land state_index) > 0) controls)
+  let controls_check (state_index: int) (controls: Circuit.adr list): bool =
+    let check (Circuit.A c) = (1 lsl c) land state_index > 0 in
+    List.(fold_left (&&) true (map check controls))
+
+  type matrix = Complex.t * Complex.t * Complex.t * Complex.t
+
+  let cpx_2 =
+    Complex.{ re = 2.0; im = 0.0 }
+
+  let cpx_inv_sqrt_2 =
+    Complex.(div one (sqrt cpx_2))
+
+  let cpx_2_pi =
+    Complex.{ re = 2.0 *. Float.pi; im = 0.0 }
+
+  let cpx_pow_2 m =
+    Complex.(pow cpx_2 { re = float_of_int m; im = 0.0 })
     
-  let matrix_for_gate (g: Circuit.gate_kind): Complex.t * Complex.t * Complex.t * Complex.t = match g with
+  let matrix_for_gate (g : Circuit.gate_kind) : matrix =
+    match g with
     | X -> Complex.(zero, one, one, zero)
     | Z -> Complex.(one, zero, zero, neg one)
-    | H -> (one_over_sqrt_2, one_over_sqrt_2, one_over_sqrt_2, Complex.neg one_over_sqrt_2)
-    | Rm m -> Complex.(one, zero, zero, exp (div (mul two_pi i) (pow complex_two {re = float_of_int m; im = 0.0})))
+    | H -> (cpx_inv_sqrt_2, cpx_inv_sqrt_2, cpx_inv_sqrt_2, Complex.neg cpx_inv_sqrt_2)
+    | Rm m -> Complex.(one, zero, zero, exp (div (mul cpx_2_pi i) (cpx_pow_2 m)))
 
   let apply_gate (g : Circuit.gate) (state : qstate) = 
     let iterations = Array.length state / 2 in
@@ -39,13 +53,13 @@ module NaiveBackend : BACKEND with type qstate = Complex.t array = struct
     let (A t) = g.target in
 
     for i = 0 to iterations - 1 do
-      let (firstIndex, secondIndex) = iterationIndices i t in
-      let first = Array.get state firstIndex in
-      let second = Array.get state secondIndex in
-      let check = controls_check firstIndex g.controls in
+      let (i1, i2) = iteration_indices i t in
+      let c1 = state.(i1) in
+      let c2 = state.(i2) in
+      let check = controls_check i1 g.controls in
       if check then begin
-        Array.set state firstIndex (Complex.add (Complex.mul a first) (Complex.mul b second));
-        Array.set state secondIndex (Complex.add (Complex.mul c first) (Complex.mul d second))
+        state.(i1) <- (Complex.add (Complex.mul a c1) (Complex.mul b c2));
+        state.(i2) <- (Complex.add (Complex.mul c c1) (Complex.mul d c2))
       end
     done
 
