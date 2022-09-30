38438bf6d375f40d2f5d01c439a04314f8d334d3
Arthur Correnson <arthur.correnson@gmail.com>
2022-09-30 11:33:13 +0200

    Make resolve_qaddresses more idiomatic

--- src/compiler.ml
+++ src/compiler.ml
@@ -1,22 +1,17 @@
 open Utils
 
+type mapping = ((string * int) * int) list
+
 (* First pass: remap named registers to a single address space *)
 (* Return a pair of size of address space and register mapping *)
-let rec resolve_qaddresses (f : Qasm.stmt list) (count : int) =
+let rec resolve_qaddresses (f : Qasm.stmt list) (count : int) : int * mapping =
   match f with
-  | [] -> (0, fun n _ -> failwith @@ Printf.sprintf "qreg '%s' does not exist" n)
+  | [] -> (0, [])
   | Qasm.Qreg (Qasm.Id name, Qasm.Nnint sz) :: tail ->
-    let count' = count + sz in
-    let address_for = (fun (name' : string) (idx : int) ->
-        if name = name'
-        then if idx < sz
-          then count + idx
-          else failwith @@ Printf.sprintf "index %d is out of bounds, qreg '%s' was previously declared with size %d" idx name sz
-        else
-          let (_, address_for') = (resolve_qaddresses tail count') in
-          address_for' name' idx
-      ) in
-    (count', address_for)
+    let count1 = count + sz in
+    let map1 = List.init sz (fun i -> ((name, i), count + i)) in
+    let (count2, map2) = resolve_qaddresses tail count1 in
+    (count2, map1 @ map2)
   | _ :: tail -> resolve_qaddresses tail count
 
 (* TODO: turn a uop into a Circuit.gate *)
@@ -33,9 +28,6 @@ let rec compile_gates (f : Qasm.stmt list) address_for =
 
 
 let compile (prog : Qasm.t) : Circuit.t =
-  let (sz, address_for) = resolve_qaddresses prog.body 0 in
-  let gates = compile_gates prog.body address_for in
-  {
-    qbits = sz;
-    gates = gates;
-  }
+  let (qbits, mapping) = resolve_qaddresses prog.body 0 in
+  let gates = compile_gates prog.body mapping in
+  { qbits; gates; }
