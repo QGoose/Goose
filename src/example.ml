b7f92cf09d3eda4ff8eac2b76ae41f6a33b0704d
Youssef Moawad <youssef_programming@icloud.com>
2022-10-21 12:58:05 +0100

    Adds qft generating function

--- src/example.ml
+++ src/example.ml
@@ -6,17 +6,15 @@ let superposition n = {qbits = n; gates = List.init n (fun i -> {target = (A i);
 
 let entanglement n = {qbits = n; gates = List.cons ({target = (A 0); kind = H; controls = []}) (List.init (n-1) (fun i -> {target = (A (i+1)); kind = X; controls = [A (0)]}))}
 
-let qft = {
-  qbits = 2;
-  gates = [
-    {target = A 0; kind = H; controls = []};
-    {target = A 0; kind = Rm 2; controls = [A 1]};
-    {target = A 1; kind = H; controls = []};
-  ]
+let qft n = {
+  qbits = n;
+  gates = List.concat (List.init n (fun i -> 
+    List.cons 
+    ({target = (A i); kind = H; controls = []}) 
+    (List.init (n-i-1) (fun j -> {target = (A i); kind = Rm (j+2); controls = [A (j+i+1)]}))));
 }
 
 let superpos_res = NaiveSimulator.run (superposition 10)
 let entanglement_res = NaiveSimulator.run (entanglement 3)
 
 let entanglement_se_res = SE_Engine.run (entanglement 3)
-
