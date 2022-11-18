c07b665d198d011f7f56ab676070a76e8f55fbe5
Youssef Moawad <youssef_programming@icloud.com>
2022-11-18 17:11:35 +0000

    Simplifies C emission interface and adds some brief examples to README

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Compiling the project requires `opam`, `opal`, `dune`, and `ppx_inline_test` to 
 **To setup the project**
 
 ```
-cd QuantumGoose
+cd Goose
 opam install dune opal ppx_inline_test
 ```
 **To compile**
@@ -27,4 +27,74 @@ dune build
 
 ```
 dune runtest
+```
+
+## Examples
+
+### Building a Quantum Circuit in the Goose IR
+
+The following is a quantum circuit that computes [GHZ states](https://en.wikipedia.org/wiki/Greenberger%E2%80%93Horne%E2%80%93Zeilinger_state). This entangles the qubits such that when one is measured, the states of the others are immediately determined.
+
+![](ghz3.png)
+
+The following OCaml code generates an n-qubit entanglement circuit, which computes GHZ states.
+
+```ocaml
+let entanglement n = {
+  qbits = n;
+  gates = List.cons ({
+    target = A 0;
+    kind = H;
+    controls = []
+  }) (List.init (n - 1) (fun i -> {
+    target = A (i + 1);
+    kind = X;
+    controls = [A 0]
+  }))
+}
+```
+
+### C code emission
+
+The above entanglement circuit can be compiled to C using the symbolic simulator:
+
+```ocaml
+open Quantumlib
+open C_emitter
+
+CEmitter.emitc (entanglement 3) (open_out "se_c_bin/ent3.c")
+```
+
+This produces the following C code for a 3-qubit entanglement circuit:
+
+```c
+int main(int argc, char **argv) {
+	cfloat *state = (cfloat *) malloc(N * sizeof(cfloat));
+	
+	// Initialize state
+	for (int i = 0; i < N; i++) {
+		state[i] = (cfloat) {0.0, 0.0};
+	}
+	state[0] = (cfloat) {1.0, 0.0};
+
+	// Allocate output state buffer
+	cfloat *out_state = (cfloat *) malloc(N * sizeof(cfloat));
+	
+	out_state[0] = cmul(SQRT1_2,cadd(state[0],state[1]));
+	out_state[1] = cmul(SQRT1_2,csub(state[6],state[7]));
+	out_state[2] = cmul(SQRT1_2,cadd(state[2],state[3]));
+	out_state[3] = cmul(SQRT1_2,csub(state[4],state[5]));
+	out_state[4] = cmul(SQRT1_2,cadd(state[4],state[5]));
+	out_state[5] = cmul(SQRT1_2,csub(state[2],state[3]));
+	out_state[6] = cmul(SQRT1_2,cadd(state[6],state[7]));
+	out_state[7] = cmul(SQRT1_2,csub(state[0],state[1]));
+	
+
+	// Print state
+	for (int i = 0; i < N; i++) {
+		printf("|%d>: %f + %fi\n", i, out_state[i].real, out_state[i].imag);
+	}	
+
+	return 0;
+}
 ```
--- src/c_emitter.ml
+++ src/c_emitter.ml
@@ -4,27 +4,36 @@ open Symbolic
 (* The CEmitter uses a symbex backend to evaluate a circuit and outputs pure C code based on a template. *)
 module CEmitter : sig
   val emit  : SBackend.qstate -> string
-  val process_template : string -> string -> SBackend.qstate -> unit
+  (* val process_template : string -> string -> SBackend.qstate -> unit *)
+
+  val emitc : Circuit.t -> out_channel -> unit
 
 end = struct
   (* Returns a string representing all the updates to the state vector resulting from the circuit (the final reduced expressions). *)
   let emit se_state =  
     String.concat "" Array.(to_list (mapi (fun i s -> (Printf.sprintf "out_state[%d] = %s;\n\t" i (Expr.c_template_repr s))) se_state))
-
-  (* TODO: Rewrite this to be more readable and maintainable *)
-  (* Processes a template file and outputs the C code to the given file by emitting the update code of the given symbolic state vector. *)
-  let process_template template_file output_file se_state =
+      
+  (* Emits C code for a given quantum circuit to the provided output channel. *)
+  let emitc circ out = 
+    let se_res = SE_Engine.run circ in
     let lines file =
       let contents = In_channel.with_open_bin file In_channel.input_all in
-      String.split_on_char '\n' contents in
-    let write_lines file lines =
-      let output_lines oc = output_string oc (String.concat "\n" lines) in
-      Out_channel.with_open_bin file output_lines in
-    let number_of_states = Int.to_string (Array.length se_state) in
+      String.split_on_char '\n' contents 
+    in
     
-
-    let template_lines = lines template_file in
-    let output_lines = List.map (fun line -> Str.global_replace (Str.regexp "{{N}}") number_of_states (Str.global_replace (Str.regexp "{{SymbolicExpressions}}") (emit se_state) line)) template_lines in
-    write_lines output_file output_lines 
-
+    let number_of_states = Int.to_string (Array.length se_res) in
+    
+    let template_lines = lines "src/c_template.c" in
+    
+    (* TODO: Rewrite this to be more maintainable *)
+    let output_lines = List.map (fun line ->
+      Str.global_replace
+        (Str.regexp "{{N}}")
+        number_of_states
+        (Str.global_replace (Str.regexp "{{SymbolicExpressions}}") (emit se_res) line)
+    ) template_lines 
+    in
+    
+    output_string out (String.concat "\n" output_lines);
+    flush out
 end
