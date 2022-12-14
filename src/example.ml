4b4a3771f095bd10bebc75679bc6b9cc56a64ed5
Youssef Moawad <youssef_programming@icloud.com>
2022-12-14 23:25:57 +0000

    Implemented grovers as an example; SE hangs

--- src/example.ml
+++ src/example.ml
@@ -28,6 +28,34 @@ let qft n = {
     (List.init (n-i-1) (fun j -> {target = (A i); kind = Rm (j+2); controls = [A (j+i+1)]}))));
 }
 
+let groversDiffusionList n = List.concat [
+  List.init n (fun i -> {target = (A i); kind = H; controls = []});
+  List.init n (fun i -> {target = (A i); kind = X; controls = []});
+  [{target = (A (n-1)); kind = Z; controls = (List.init (n-1) (fun i -> (A i)))}];
+  List.init n (fun i -> {target = (A i); kind = X; controls = []});
+  List.init n (fun i -> {target = (A i); kind = H; controls = []})
+]
+
+let groversOracleList n offQubits = List.concat [
+  List.map (fun q -> {target = (A q); kind = X; controls = []}) offQubits;
+  [{target = (A (n-1)); kind = Z; controls = (List.init (n-1) (fun i -> (A i)))}];
+  List.map (fun q -> {target = (A q); kind = X; controls = []}) offQubits
+]
+
+let repeat ls n =
+  let rec f l = function
+      | 0 -> l
+      | n -> f (List.rev_append ls l) (n-1) in
+  List.rev (f [] n)
+
+let groversSearch n offQubits iterations = { (* The off-qubits are indexed from the right *)
+  qbits = n;
+  gates = List.concat [
+    List.init n (fun i -> {target = (A i); kind = H; controls = []});
+    repeat (List.concat [(groversDiffusionList n); (groversOracleList n offQubits)]) iterations
+  ]
+}
+
 let superpos_res = NaiveSimulator.run (superposition 10)
 let entanglement_res = NaiveSimulator.run (entanglement 3)
 
@@ -36,3 +64,6 @@ let entanglement_se_res = SE_Engine.run (entanglement 3)
 let qft3_res = NaiveSimulator.run (qft 3);;
 
 let qft3_se_res = SE_Engine.run (qft 3);;
+
+let grovers_res = NaiveSimulator.run (groversSearch 3 [0; 1] 3);;
+let grovers_se_res = SE_Engine.run (groversSearch 3 [0; 1] 3);;
