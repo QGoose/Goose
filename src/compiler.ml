2d6a3fd38ebf20f85d55df2e00146b75d5d60679
mcncm <33966432+mcncm@users.noreply.github.com>
2022-12-20 20:03:31 -0500

    Fill in stubbed-out function, delete old code

--- src/compiler.ml
+++ src/compiler.ml
@@ -22,7 +22,6 @@ let new_addresses (_ : unit) : addresses = {
   size = 0;
 }
 
-
 (* TODO: make this namespaced by a module, where `addresses` becomes `Addresses.t` or whatever the convention is. *)
 let resolve (addrs : addresses) (name : string) (offset : int) : Circuit.adr =
   (* Why doesn't this function return a `register option`? *)
@@ -32,30 +31,6 @@ let resolve (addrs : addresses) (name : string) (offset : int) : Circuit.adr =
     failwith "Illegal register offset"
   else
     A (reg.first + offset)
-
-let rec alloc_qaddresses' (f : Qasm.stmt list) (addrs : addresses) : addresses =
-  match f with
-  | [] -> addrs
-  | Qasm.Qreg (Qasm.Id name, Qasm.Nnint sz) :: tail ->
-    let reg : register = {
-      first = addrs.size;
-      size = sz;
-    } in
-    (* FIXME: throw an error if there is a redundant register name *)
-    let _ = Hashtbl.add addrs.mapping name reg in
-    let addrs' = {
-      mapping = addrs.mapping;
-      size = addrs.size + sz;
-    } in
-    alloc_qaddresses' tail addrs'
-  | _ :: tail -> alloc_qaddresses' tail addrs
-
-(* Produce an address space map for the program *)
-let alloc_qaddresses (f : Qasm.stmt list) : addresses =
-  alloc_qaddresses' f {
-    mapping = Hashtbl.create 8;
-    size = 0;
-  }
 
 let rec collect_gate_decls' (f : Qasm.stmt list) decls = match f with
   | [] -> ()
@@ -71,57 +46,6 @@ let collect_gate_decls (f : Qasm.stmt list) =
   gate_decls
 
 let eval_float (_expr : Qasm.expr) : float = todo ()
-
-(* TODO: turn a uop into a Circuit.gate *)
-let translate_gate addrs (uop : Qasm.uop) : Circuit.gate list = match uop with
-  | U (theta :: phi :: lambda :: [], arg) ->
-    let kind : Circuit.gate_kind = U {
-        theta = eval_float theta;
-        phi = eval_float phi;
-        lambda = eval_float lambda;
-      } in
-    let tgt = match arg with
-      (* TODO: Act on all qubits in a register? *)
-      | A_id (Id _tgt_reg, None) -> failwith "Unimplemented: map gate over register"
-      (* Act on a single qubit in a register at the given offset *)
-      | A_id (Id tgt_reg, Some tgt_offset) -> resolve addrs tgt_reg (Qasm.int_of_nnint tgt_offset)
-    in
-    let (gate : Circuit.gate) = {
-      target = tgt;
-      kind = kind;
-      controls = [];
-    }
-    in [gate]
-  (* FIXME: It seems like this shouldn't be a semantic error but a parser error. *)
-  | U (_, _) -> failwith "Illegal arbitrary single qubit unitary"
-  | CX (A_id (Id _src_reg, None), A_id (Id _tgt_reg, None)) -> todo ()
-  | CX (A_id (Id src_reg, Some src_offset), A_id (Id tgt_reg, Some tgt_offset)) ->
-    let tgt = resolve addrs tgt_reg (Qasm.int_of_nnint tgt_offset) in
-    let src = resolve addrs src_reg (Qasm.int_of_nnint src_offset) in
-    {
-      target = tgt;
-      kind = X;
-      controls = [src];
-    } :: []
-  (* TODO: actual error message: trying to map over something that can't be mapped over. *)
-  | CX _ -> failwith "Illegal control operation"
-  | App (_, _, _) -> todo()
-
-(* Second pass: filter gates from Qasm AST *)
-let rec compile_gates (f : Qasm.stmt list) (addrs : addresses) =
-  match f with
-  | [] -> []
-  | Qasm.Qop (Qasm.Q_uop uop) :: tail -> (translate_gate addrs uop) @ (compile_gates tail addrs)
-  (* In this pass, ignore the `qreg` declarations *)
-  | Qasm.Qreg _ :: tail -> (compile_gates tail addrs)
-  | _ -> failwith "Unsupported instruction"
-
-let compile (prog : Qasm.t) : Circuit.t =
-  let addrs = alloc_qaddresses prog.body in
-  let gates = compile_gates prog.body addrs in
-  { qbits = addrs.size; gates; }
-
-(* starting over *)
 
 type gate = unit
 
@@ -166,8 +90,40 @@ let compile_reg_decl (addrs : addresses) (name : Qasm.id) (sz : Qasm.nnint) : ad
 let compile_gate_decl (cs : compile_state) (name : Qasm.id) (params : Qasm.id list)
     (targets : Qasm.id list) (body : Qasm.gop list) : compile_state = todo ()
 
-(* Compile unitary operation *)
-let compile_uop (cs : compile_state) (op : Qasm.uop): compile_state = todo ()
+(* Turn a uop into (possibly a list of) Circuit.gate *)
+let compile_uop (addrs : addresses) (uop : Qasm.uop) : Circuit.gate list = match uop with
+  | U (theta :: phi :: lambda :: [], arg) ->
+    let kind : Circuit.gate_kind = U {
+        theta = eval_float theta;
+        phi = eval_float phi;
+        lambda = eval_float lambda;
+      } in
+    let tgt = match arg with
+      (* TODO: Act on all qubits in a register? *)
+      | A_id (Id _tgt_reg, None) -> failwith "Unimplemented: map gate over register"
+      (* Act on a single qubit in a register at the given offset *)
+      | A_id (Id tgt_reg, Some tgt_offset) -> resolve addrs tgt_reg (Qasm.int_of_nnint tgt_offset)
+    in
+    let (gate : Circuit.gate) = {
+      target = tgt;
+      kind = kind;
+      controls = [];
+    }
+    in [gate]
+  (* FIXME: It seems like this shouldn't be a semantic error but a parser error. *)
+  | U (_, _) -> failwith "Illegal arbitrary single qubit unitary"
+  | CX (A_id (Id _src_reg, None), A_id (Id _tgt_reg, None)) -> todo ()
+  | CX (A_id (Id src_reg, Some src_offset), A_id (Id tgt_reg, Some tgt_offset)) ->
+    let tgt = resolve addrs tgt_reg (Qasm.int_of_nnint tgt_offset) in
+    let src = resolve addrs src_reg (Qasm.int_of_nnint src_offset) in
+    {
+      target = tgt;
+      kind = X;
+      controls = [src];
+    } :: []
+  (* TODO: actual error message: trying to map over something that can't be mapped over. *)
+  | CX _ -> failwith "Illegal control operation"
+  | App (_, _, _) -> todo()
 
 let compile_stmt (cs : compile_state) (stmt : Qasm.stmt) : compile_state = match stmt with
   (* Compile register declaration *)
@@ -183,7 +139,11 @@ let compile_stmt (cs : compile_state) (stmt : Qasm.stmt) : compile_state = match
   (* Compile gate declaration *)
   | GateDecl (name, params, targets, body) -> compile_gate_decl cs name params targets body
   (* Compile uop *)
-  | Qasm.Qop (Qasm.Q_uop uop) -> compile_uop cs uop
+  | Qasm.Qop (Qasm.Q_uop uop) -> let new_gates = compile_uop cs.env.addrs uop in
+    {
+      env = cs.env;
+      gates = cs.gates @ new_gates;
+    }
   | _ -> todo ()
 
 let compile' (prog : Qasm.t) : Circuit.t =
