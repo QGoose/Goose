2a02780b6ff02b60212ea6e005c92a44c55da62b
mcncm <33966432+mcncm@users.noreply.github.com>
2023-01-31 14:07:09 +0100

    Implemented arb gate for SE

--- src/symbolic.ml
+++ src/symbolic.ml
@@ -6,12 +6,12 @@
 
 (** Manipulation of symbolic expressions. *)
 module Expr = struct
-  
+
   (** Type of symbolic expressions. *)
   type t =
     | Bop of Qasm.binaryop * t * t
     | Uop of Qasm.unaryop * t
-    | Cst of int
+    | Cst of float
     | Pi
     | I
     | Var of int
@@ -19,7 +19,7 @@ module Expr = struct
 
   (** Symbolic product. *)
   let ( *! ) x y = Bop (Qasm.MUL, x, y)
-  
+
   (** Symbolic sum. *)
   let ( +! ) x y = Bop (Qasm.ADD, x, y)
 
@@ -54,7 +54,7 @@ module Expr = struct
     | Uop (op, e) ->
       Printf.sprintf "(%s %s)" (Qasm.string_of_unary op) (repr e)
     | Cst c ->
-      Printf.sprintf "Cst %d" c
+      Printf.sprintf "Cst %f" c
     | Pi -> Printf.sprintf "pi"
     | I -> Printf.sprintf "i"
     | Var v ->
@@ -75,20 +75,20 @@ module Expr = struct
     | Bop (Qasm.ADD, Bop (Qasm.MUL, CustomSymbol s0, e0), Bop (Qasm.MUL, Uop (Qasm.NEG, CustomSymbol s1), e1)) when s0 = s1 ->
       Bop (Qasm.MUL, CustomSymbol s0, Bop (Qasm.SUB, reduce e0, reduce e1))
 
-    | Bop (Qasm.MUL, Cst 0, _) -> Cst 0
-    | Bop (Qasm.MUL, _, Cst 0) -> Cst 0
-    | Bop (Qasm.MUL, Cst 1, e) -> reduce e
-    | Bop (Qasm.MUL, e, Cst 1) -> reduce e
-    | Bop (Qasm.ADD, Cst 0, e) -> reduce e
-    | Bop (Qasm.ADD, e, Cst 0) -> reduce e
-    | Bop (Qasm.SUB, Cst 0, e) -> neg (reduce e)
-    | Bop (Qasm.SUB, e, Cst 0) -> reduce e
-    | Bop (Qasm.DIV, Cst 0, _) -> Cst 0
-    | Bop (Qasm.DIV, e, Cst 1) -> reduce e
-    | Bop (Qasm.POW, Cst 0, _) -> Cst 0
-    | Bop (Qasm.POW, _, Cst 0) -> Cst 1
-    | Bop (Qasm.POW, Cst 1, _) -> Cst 1
-    | Bop (Qasm.POW, e, Cst 1) -> reduce e
+    | Bop (Qasm.MUL, Cst 0., _) -> Cst 0.
+    | Bop (Qasm.MUL, _, Cst 0.) -> Cst 0.
+    | Bop (Qasm.MUL, Cst 1., e) -> reduce e
+    | Bop (Qasm.MUL, e, Cst 1.) -> reduce e
+    | Bop (Qasm.ADD, Cst 0., e) -> reduce e
+    | Bop (Qasm.ADD, e, Cst 0.) -> reduce e
+    | Bop (Qasm.SUB, Cst 0., e) -> neg (reduce e)
+    | Bop (Qasm.SUB, e, Cst 0.) -> reduce e
+    | Bop (Qasm.DIV, Cst 0., _) -> Cst 0.
+    | Bop (Qasm.DIV, e, Cst 1.) -> reduce e
+    | Bop (Qasm.POW, Cst 0., _) -> Cst 0.
+    | Bop (Qasm.POW, _, Cst 0.) -> Cst 1.
+    | Bop (Qasm.POW, Cst 1., _) -> Cst 1.
+    | Bop (Qasm.POW, e, Cst 1.) -> reduce e
 
     (* Base cases *)
     | Bop (op, e1, e2) -> Bop (op, reduce e1, reduce e2)
@@ -112,7 +112,7 @@ module EExpr = struct
     | Ei
     | Epi
     | Evar of int
-    | Ecst of int
+    | Ecst of float
     | Esym of string
     | Ebop of Qasm.binaryop * int * int
     | Euop of Qasm.unaryop * int
@@ -141,8 +141,8 @@ module LExpr = struct
   type t = { bindings : (string * Expr.t) list; value : Expr.t }
   let print out { bindings; value } =
     List.iter (fun (x, e) ->
-      Printf.fprintf out "let %s = %s in\n" x (Expr.repr e)
-    ) bindings;
+        Printf.fprintf out "let %s = %s in\n" x (Expr.repr e)
+      ) bindings;
     Printf.printf "%s\n" (Expr.repr value)
 end
 
@@ -188,8 +188,8 @@ module Egraph = struct
     match (get id) with
     | Ebop (Qasm.MUL, x1, x2) -> Some (x1, x2)
     | _ -> None
-  
-  let is_neg (id : int) : int option = 
+
+  let is_neg (id : int) : int option =
     match (get id) with
     | Euop (Qasm.NEG, x) -> Some x
     | _ -> None
@@ -202,50 +202,50 @@ module Egraph = struct
   let mk_cst c = register (Ecst c)
   let mk_var v = register (Evar v)
   let mk_custom s = register (Esym s)
-  
-  let rec mk_add x1 x2 = 
-    if x1 = x2 then mk_mul (mk_cst 2) x1
-    else if x1 = mk_cst 0 then x2
-    else if x2 = mk_cst 0 then x1
+
+  let rec mk_add x1 x2 =
+    if x1 = x2 then mk_mul (mk_cst 2.) x1
+    else if x1 = mk_cst 0. then x2
+    else if x2 = mk_cst 0. then x1
     else match (is_mul x1, is_mul x2) with
-    | Some (a, b), Some (c, d) when a = c -> mk_mul a (mk_add b d)
-    | Some (a, b), Some (c, d) when a = d -> mk_mul a (mk_add b c)
-    | Some (a, b), Some (c, d) when b = c -> mk_mul b (mk_add a d)
-    | Some (a, b), Some (c, d) when b = d -> mk_mul b (mk_add a c)
-    | _ -> register (Ebop (ADD, x1, x2))
+      | Some (a, b), Some (c, d) when a = c -> mk_mul a (mk_add b d)
+      | Some (a, b), Some (c, d) when a = d -> mk_mul a (mk_add b c)
+      | Some (a, b), Some (c, d) when b = c -> mk_mul b (mk_add a d)
+      | Some (a, b), Some (c, d) when b = d -> mk_mul b (mk_add a c)
+      | _ -> register (Ebop (ADD, x1, x2))
 
   and mk_mul x1 x2 = 
-    let c0 = mk_cst 0 in
-    let c1 = mk_cst 1 in
-    if x1 = x2 then mk_pow x1 (mk_cst 2)
+    let c0 = mk_cst 0. in
+    let c1 = mk_cst 1. in
+    if x1 = x2 then mk_pow x1 (mk_cst 2.)
     else if x1 = c0 || x2 = c0 then c0
     else if x1 = c1 then x2 
     else if x2 = c1 then x1
     else register (Ebop (MUL, x1, x2))
-    
-  and mk_sub x1 x2 = 
-    let c0 = mk_cst 0 in
+
+  and mk_sub x1 x2 =
+    let c0 = mk_cst 0. in
     if x1 = x2 then c0
     else if x1 = c0 then mk_neg x2
     else if x2 = c0 then x1
     else match (is_mul x1, is_mul x2) with
-    | Some (a, b), Some (c, d) when a = c -> mk_mul a (mk_sub b d)
-    | Some (a, b), Some (c, d) when a = d -> mk_mul a (mk_sub b c)
-    | Some (a, b), Some (c, d) when b = c -> mk_mul b (mk_sub a d)
-    | Some (a, b), Some (c, d) when b = d -> mk_mul b (mk_sub a c)
-    | _ -> register (Ebop (SUB, x1, x2))
+      | Some (a, b), Some (c, d) when a = c -> mk_mul a (mk_sub b d)
+      | Some (a, b), Some (c, d) when a = d -> mk_mul a (mk_sub b c)
+      | Some (a, b), Some (c, d) when b = c -> mk_mul b (mk_sub a d)
+      | Some (a, b), Some (c, d) when b = d -> mk_mul b (mk_sub a c)
+      | _ -> register (Ebop (SUB, x1, x2))
 
   and mk_div x1 x2 = 
-    let c0 = mk_cst 0 in
-    let c1 = mk_cst 1 in
+    let c0 = mk_cst 0. in
+    let c1 = mk_cst 1. in
     if x1 = c0 then c0
     else if x2 = c1 then x1
     else if x1 = c1 then mk_inv x2
     else register (Ebop (DIV, x1, x2))
 
   and mk_pow x1 x2 = 
-    let c0 = mk_cst 0 in
-    let c1 = mk_cst 1 in
+    let c0 = mk_cst 0. in
+    let c1 = mk_cst 1. in
     if x1 = c0 then c0 
     else if x2 = c0 then c1
     else if x1 = c1 then c1
@@ -254,17 +254,17 @@ module Egraph = struct
 
   and mk_sqrt x = register (Euop (SQRT, x))
   and mk_exp x = register (Euop (EXP, x))
-  
-  and mk_inv x = 
-    let c1 = mk_cst 1 in
-    let c2 = mk_cst 2 in
+
+  and mk_inv x =
+    let c1 = mk_cst 1. in
+    let c2 = mk_cst 2. in
     if x = c1 then c1 
     else match (is_sqrt x) with 
-    | Some sqrted when sqrted = c2 -> mk_custom "SQRT1_2"
-    | _ -> register (Euop (INV, x))
+      | Some sqrted when sqrted = c2 -> mk_custom "SQRT1_2"
+      | _ -> register (Euop (INV, x))
 
   and mk_neg x = 
-    let c0 = mk_cst 0 in
+    let c0 = mk_cst 0. in
     if x = c0 then c0
     else register (Euop (NEG, x))
 end
